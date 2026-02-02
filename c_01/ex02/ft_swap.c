4435cd019a9ab2197fd565f44615a2ec73f4dde7
Louis Wicket <louis.wicket@protonmail.com>
2026-02-02 23:31:25 +0100

    start c01

---
+++ c_01/ex02/ft_swap.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lwicket <louis.wicket@protonmail.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/02 23:22:02 by lwicket           #+#    #+#             */
+/*   Updated: 2026/02/02 23:27:26 by lwicket          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifdef TEST
+# include <assert.h>
+#endif
+
+void	ft_swap(int *a, int *b)
+{
+	const int	tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
+
+#ifdef TEST
+
+// run with test main:
+// clang ft_swap.c -W{all,extra,error} -DTEST && ./a.out
+int	main(void)
+{
+	int	a;
+	int	b;
+
+	a = 'A';
+	b = 'B';
+	ft_swap(&a, &b);
+	assert(a == 'B' && b == 'A');
+}
+
+#endif
