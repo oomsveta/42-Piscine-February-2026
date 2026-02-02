4098a6ca31ed90b7de9f652e091fe9dcf45f398c
Louis Wicket <lwicket@fu-r8-p2.s19.be>
2026-02-02 12:12:14 +0100

    shell00: add ex01

---
+++ shell_00/ex01/.solve.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+rm --force -- testShell00*
+
+# -r--r-xr-x 1 XX XX 40 Jun 1 23:42 testShell00
+truncate --size 40 testShell00
+chmod u=r,g=rx,o=rx testShell00
+touch --date "Jun 1 23:42" testShell00
+
+echo "Obtained:"
+ls -l
+echo "Expected:"
+echo "-r--r-xr-x 1 XX XX 40 Jun 1 23:42 testShell00"
+
+tar --create --file testShell00.tar testShell00
+rm --force testShell00
