3af38825452f5aec6fb8ef16d6bf9a323a86faf0
Louis Wicket <louis.wicket@protonmail.com>
2026-02-07 21:11:07 +0100

    add ft_split, complete c07

---
+++ c_07/ex05/ft_split.c
@@ -0,0 +1,124 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lwicket <louis.wicket@protonmail.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/07 19:09:05 by lwicket           #+#    #+#             */
+/*   Updated: 2026/02/07 21:10:10 by lwicket          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>	// provides free, malloc, NULL, size_t
+
+static size_t	ft_strspn(const char *str, const char *accept)
+{
+	size_t	len;
+	size_t	i;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		i = 0;
+		while (accept[i] != '\0' && accept[i] != str[len])
+		{
+			i += 1;
+		}
+		if (accept[i] == '\0')
+		{
+			break ;
+		}
+		len += 1;
+	}
+	return (len);
+}
+
+static size_t	ft_strcspn(const char *str, const char *reject)
+{
+	size_t	i;
+	size_t	len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		i = 0;
+		while (reject[i] != '\0')
+		{
+			if (reject[i] == str[len])
+			{
+				return (len);
+			}
+			i += 1;
+		}
+		len += 1;
+	}
+	return (len);
+}
+
+static char	*ft_strndup(const char *str, size_t n)
+{
+	size_t	i;
+	size_t	len;
+	char	*dup;
+
+	len = 0;
+	while (len < n && str[len] != '\0')
+	{
+		len += 1;
+	}
+	dup = malloc(len + 1);
+	dup[len] = '\0';
+	if (!dup)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < len)
+	{
+		dup[i] = str[i];
+		i += 1;
+	}
+	return (dup);
+}
+
+static size_t	count_words(const char *str, const char *separators)
+{
+	size_t	nbr_of_words;
+
+	str += ft_strspn(str, separators);
+	nbr_of_words = 0;
+	while (*str != '\0')
+	{
+		nbr_of_words += 1;
+		str += ft_strcspn(str, separators);
+		str += ft_strspn(str, separators);
+	}
+	return (nbr_of_words);
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	const size_t	nbr_of_words = count_words(str, charset);
+	char			**words;
+	size_t			i;
+	size_t			substr_len;
+
+	words = malloc(sizeof(char *) * (nbr_of_words + 1));
+	if (!words)
+	{
+		return (NULL);
+	}
+	words[nbr_of_words] = NULL;
+	str += ft_strspn(str, charset);
+	i = 0;
+	while (i < nbr_of_words)
+	{
+		substr_len = ft_strcspn(str, charset);
+		words[i] = ft_strndup(str, substr_len);
+		str += substr_len;
+		str += ft_strspn(str, charset);
+		i += 1;
+	}
+	return (words);
+}
