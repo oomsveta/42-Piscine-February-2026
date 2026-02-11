6343b73ee9ddb750d31c034d1d7842a4ca4a49de
Louis Wicket <louis.wicket@protonmail.com>
2026-02-11 08:00:58 +0100

    add ft_is_sort, start do-op

---
+++ c_11/ex05/ops.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ops.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lwicket <louis.wicket@protonmail.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/10 21:56:09 by lwicket           #+#    #+#             */
+/*   Updated: 2026/02/10 21:59:40 by lwicket          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	add(int a, int b)
+{
+	return (a + b);
+}
+
+int	sub(int a, int b)
+{
+	return (a - b);
+}
+
+int	mul(int a, int b)
+{
+	return (a * b);
+}
+
+int	div(int a, int b)
+{
+	return (a / b);
+}
+
+int	mod(int a, int b)
+{
+	return (a % b);
+}
