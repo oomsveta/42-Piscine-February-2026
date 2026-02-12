19d705453954079b6b3123961dd36fdc541f4404
Louis Wicket <louis.wicket@protonmail.com>
2026-02-12 18:53:09 +0100

    add btree_create_node to ft_btree.h

--- c_13/ex01/ft_btree.h
+++ c_13/ex01/ft_btree.h
@@ -6,7 +6,7 @@
 /*   By: lwicket <louis.wicket@protonmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 13:12:02 by lwicket           #+#    #+#             */
-/*   Updated: 2026/02/12 13:12:29 by lwicket          ###   ########.fr       */
+/*   Updated: 2026/02/12 18:52:09 by lwicket          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,5 @@ typedef struct s_btree
 	struct s_btree	*right;
 	void			*item;
 }	t_btree;
+
+t_btree	*btree_create_node(void *item);
