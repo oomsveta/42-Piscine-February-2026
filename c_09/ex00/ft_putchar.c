b61320061cd49e89bda2bf3c6589babbc04952bb
Louis Wicket <lwicket@fu-r9-p7.s19.be>
2026-02-12 16:41:06 +0100

    fix: adapt prototype to exo requirements
    exo's putchar receives a char, not an int

--- c_09/ex00/ft_putchar.c
+++ c_09/ex00/ft_putchar.c
@@ -6,15 +6,13 @@
 /*   By: lwicket <louis.wicket@protonmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 19:20:28 by lwicket           #+#    #+#             */
-/*   Updated: 2026/02/08 12:49:33 by lwicket          ###   ########.fr       */
+/*   Updated: 2026/02/12 16:40:47 by lwicket          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h> // provides STDOUT_FILENO, write
 
-void	ft_putchar(int c)
+void	ft_putchar(char c)
 {
-	const unsigned char	byte = (const unsigned char)c;
-
-	write(STDOUT_FILENO, &byte, sizeof byte);
+	write(STDOUT_FILENO, &c, sizeof c);
 }
