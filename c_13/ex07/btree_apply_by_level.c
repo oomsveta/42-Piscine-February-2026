8cea75ea3d23ad92b66da356775d865fa74658bc
Louis Wicket <louis.wicket@protonmail.com>
2026-02-16 21:13:18 +0100

    complete btree_apply_by_level

--- c_13/ex07/btree_apply_by_level.c
+++ c_13/ex07/btree_apply_by_level.c
@@ -6,37 +6,106 @@
 /*   By: lwicket <louis.wicket@protonmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/15 11:32:38 by lwicket           #+#    #+#             */
-/*   Updated: 2026/02/15 11:49:28 by lwicket          ###   ########.fr       */
+/*   Updated: 2026/02/16 21:09:25 by lwicket          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_btree.h"	// provides t_btree
+#include <stdlib.h>		// provides malloc, size_t
+#include <stdbool.h>	// provides false, true
+#include "ft_btree.h"	// provides t_bfs_node, t_btree, t_queue
 
-static int	ft_max(int a, int b)
+static size_t	count_nodes(t_btree *root)
 {
-	if (a >= b)
-	{
-		return (a);
-	}
-	return (b);
-}
-
-static int btree_level_count(t_btree *root)
-{
-	int	left_count;
-	int	right_count;
-
 	if (!root)
 	{
 		return (0);
 	}
-	left_count = btree_level_count(root->left);
-	right_count = btree_level_count(root->right);
-	return (1 + ft_max(left_count, right_count));
+	return (1 + count_nodes(root->left) + count_nodes(root->right));
 }
 
-
-void btree_apply_by_level(t_btree *root, void (*applyf)(void *item, int current_level, int is_first_elem))
+static void	enqueue(t_queue *queue, t_bfs_node node)
 {
-	
+	queue->nodes[queue->rear] = node;
+	queue->rear = (queue->rear + 1) % queue->capacity;
+	queue->length += 1;
 }
+
+static t_bfs_node	dequeue(t_queue *queue)
+{
+	t_bfs_node	node;
+
+	node = queue->nodes[queue->front];
+	queue->front = (queue->front + 1) % queue->capacity;
+	queue->length -= 1;
+	return (node);
+}
+
+static t_queue	*init_queue(t_queue *queue, size_t capacity)
+{
+	*queue = (t_queue){0};
+	queue->capacity = capacity;
+	queue->nodes = malloc(sizeof(t_bfs_node) * capacity);
+	if (!queue->nodes)
+	{
+		return (NULL);
+	}
+	return (queue);
+}
+
+void	btree_apply_by_level(
+	t_btree *root,
+	void (*applyf)(void *item, int current_level, int is_first_elem)
+)
+{
+	t_queue		queue;
+	t_bfs_node	node;
+	size_t		current_level;
+
+	if (!init_queue(&queue, count_nodes(root)))
+		return ;
+	enqueue(&queue, (t_bfs_node){root, 0});
+	current_level = (size_t)-1;
+	while (queue.length > 0)
+	{
+		node = dequeue(&queue);
+		if (node.level != current_level)
+		{
+			current_level = node.level;
+			applyf(node.node->item, node.level, true);
+		}
+		else
+			applyf(node.node->item, node.level, false);
+		if (node.node->left)
+			enqueue(&queue, (t_bfs_node){node.node->left, node.level + 1});
+		if (node.node->right)
+			enqueue(&queue, (t_bfs_node){node.node->right, node.level + 1});
+	}
+	free(queue.nodes);
+}
+
+/* 
+#include <stdio.h>
+
+void print_node(void *item, int level, int is_first)
+{
+	printf(
+		"%s at level %d %s\n",
+		(char *)item,
+		level,
+		is_first ? " (first in level)" : ""
+	);
+}
+
+int main(void)
+{
+	t_btree root, node1, node2, node3, node4;
+
+	root = (t_btree){&node1, &node2, "Root"};
+	node1 = (t_btree){&node3, &node4, "Node 1"};
+	node2 = (t_btree){NULL, NULL, "Node 2"};
+	node3 = (t_btree){NULL, NULL, "Node 3"};
+	node4 = (t_btree){NULL, NULL, "Node 4"};
+
+	btree_apply_by_level(&root, print_node);
+}
+*/
--- c_13/ex07/ft_btree.h
+++ c_13/ex07/ft_btree.h
@@ -6,9 +6,11 @@
 /*   By: lwicket <louis.wicket@protonmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 13:12:02 by lwicket           #+#    #+#             */
-/*   Updated: 2026/02/12 18:54:16 by lwicket          ###   ########.fr       */
+/*   Updated: 2026/02/16 20:29:12 by lwicket          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stddef.h>
 
 typedef struct s_btree
 {
@@ -16,5 +18,20 @@ typedef struct s_btree
 	struct s_btree	*right;
 	void			*item;
 }	t_btree;
+
+typedef struct s_bfs_node
+{
+	t_btree	*node;
+	size_t	level;
+}	t_bfs_node;
+
+typedef struct s_queue
+{
+	t_bfs_node	*nodes;
+	size_t		front;
+	size_t		rear;
+	size_t		capacity;
+	size_t		length;
+}	t_queue;
 
 t_btree	*btree_create_node(void *item);
