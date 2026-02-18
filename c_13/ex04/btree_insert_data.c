26215c197ded01ea7a1f19dfd66b918ab5e2a317
Louis Wicket <louis.wicket@protonmail.com>
2026-02-18 17:02:56 +0100

    c13: add tests

--- c_13/ex04/btree_insert_data.c
+++ c_13/ex04/btree_insert_data.c
@@ -6,9 +6,16 @@
 /*   By: lwicket <louis.wicket@protonmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 18:47:08 by lwicket           #+#    #+#             */
-/*   Updated: 2026/02/16 21:32:05 by lwicket          ###   ########.fr       */
+/*   Updated: 2026/02/18 17:02:38 by lwicket          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifdef TEST
+# include <assert.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+#endif
 
 #include "ft_btree.h"	// provides btree_create_node, t_btree
 
@@ -29,3 +36,51 @@ void	btree_insert_data(
 	}
 	*root = btree_create_node(item);
 }
+
+#ifdef TEST
+
+t_btree	*btree_create_node(void *item)
+{
+	t_btree	*node;
+
+	node = malloc(sizeof(t_btree));
+	if (!node)
+	{
+		return (NULL);
+	}
+	*node = (t_btree){.item = item, .left = NULL, .right = NULL};
+	return (node);
+}
+
+int	cmp(void *a, void *b)
+{
+	return (strcmp((char *)a, (char *)b));
+}
+
+// clang btree_insert_data.c -W{all,extra,error} -DTEST && ./a.out
+int	main(void)
+{
+	t_btree	*root;
+
+	root = NULL;
+	btree_insert_data(&root, "5", cmp);
+	assert(root != NULL);
+	assert(strcmp(root->item, "5") == 0);
+	assert(root->left == NULL);
+	assert(root->right == NULL);
+	btree_insert_data(&root, "3", cmp);
+	assert(root->left != NULL);
+	assert(strcmp(root->left->item, "3") == 0);
+	btree_insert_data(&root, "7", cmp);
+	assert(root->right != NULL);
+	assert(strcmp(root->right->item, "7") == 0);
+	btree_insert_data(&root, "1", cmp);
+	assert(root->left->left != NULL);
+	assert(strcmp(root->left->left->item, "1") == 0);
+	btree_insert_data(&root, "5", cmp);
+	assert(root->right->left != NULL);
+	assert(strcmp(root->right->left->item, "5") == 0);
+	puts("✅ All tests passed");
+}
+
+#endif
--- c_13/ex05/btree_search_item.c
+++ c_13/ex05/btree_search_item.c
@@ -6,9 +6,15 @@
 /*   By: lwicket <louis.wicket@protonmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 18:58:08 by lwicket           #+#    #+#             */
-/*   Updated: 2026/02/16 21:32:26 by lwicket          ###   ########.fr       */
+/*   Updated: 2026/02/18 16:57:52 by lwicket          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifdef TEST
+# include <assert.h>
+# include <stdio.h>
+# include <string.h>
+#endif
 
 #include <stddef.h>		// provides NULL
 #include "ft_btree.h"	// provides t_btree
@@ -34,3 +40,35 @@ void	*btree_search_item(
 	}
 	return (btree_search_item(root->right, data_ref, cmpf));
 }
+
+#ifdef TEST
+
+int	cmp(void *a, void *b)
+{
+	return (strcmp((char *)a, (char *)b));
+}
+
+// clang btree_search_item.c -W{all,extra,error} && ./a.out
+// (doesn't test in-order traversal)
+int	main(void)
+{
+	t_btree	a;
+	t_btree	b;
+	t_btree	c;
+	t_btree	d;
+	t_btree	e;
+
+	a = (t_btree){.left = &b, .right = &c, .item = "5"};
+	b = (t_btree){.left = &d, .right = &e, .item = "3"};
+	c = (t_btree){.left = NULL, .right = NULL, .item = "4"};
+	d = (t_btree){.left = NULL, .right = NULL, .item = "1"};
+	e = (t_btree){.left = NULL, .right = NULL, .item = "2"};
+	assert(strcmp(btree_search_item(&a, "1", cmp), "1") == 0);
+	assert(strcmp(btree_search_item(&a, "5", cmp), "5") == 0);
+	assert(strcmp(btree_search_item(&a, "3", cmp), "3") == 0);
+	assert(btree_search_item(&a, "42", cmp) == NULL);
+	assert(btree_search_item(NULL, "5", cmp) == NULL);
+	puts("✅ All tests passed");
+}
+
+#endif
